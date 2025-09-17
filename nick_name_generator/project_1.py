e5eebf5e025eece2ec0d0f0c58c4c7d47c06015b
tumblinger <77502878+tumblinger@users.noreply.github.com>
2025-09-16 20:47:23 -0400

    Create project_1.py

---
+++ nick_name_generator/project_1.py
@@ -0,0 +1,4 @@
+print('Welcome to the Funny NickName Generator!')
+color = input('What is your favorite color?\n')
+pet = input('What is your favorite pet's name?\n')
+print('Your nickname is: ' + color + '' + pet)
